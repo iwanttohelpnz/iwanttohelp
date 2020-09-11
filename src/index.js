58cf73631327fc1c23576f54bd7683741aecdf6a
vanessavps <vanessa.vps29@gmail.com>
2020-09-11 20:54:15 +1200

    Adding winston logger

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 const express = require('express')
+const logger = require('./util/logger.js')
 
 const app = express()
-
 const port = process.env.PORT || 3000
 
 app.use(express.json())
 
 app.listen(port, () => {
-    console.log('Server is up on port ' + port)
+    logger.info('Server is up on port ' + port)
 })

012834fb4ae6e81c9f8bf8bde8c19ca8dd3b0c31
vanessavps <vanessa.vps29@gmail.com>
2020-09-16 19:00:41 +1200

    Add materialize-css and handlebars support

--- src/app.js
+++ src/app.js
@@ -1,9 +1,28 @@
 const express = require('express')
+const path = require('path')
+const hbs = require('hbs')
+
 const charityRouter = require('./routers/charity')
+const indexRouter = require('./routers/index')
+
+//Define paths to express config
+const publicDirPath = path.join(__dirname, '../public')
+const viewsPath = path.join(__dirname, '../public/templates/views')
+const partialsPath = path.join(__dirname, '../public/templates/partials')
 
 const app = express()
 
+//Setup handlebars engine and view location
+app.set('view engine', 'hbs')
+app.set('views', viewsPath)
+hbs.registerPartials(partialsPath)
+
+//Setup static directory to serve
+app.use(express.static(publicDirPath))
 app.use(express.json())
+
+//Routers
 app.use(charityRouter)
+app.use(indexRouter)
 
 module.exports = app
